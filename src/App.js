f1dbb7759e61100690b920eb81cd76e6603fe452
Stefano Pisani <stefano.pisani92@gmail.com>
2021-05-05 15:37:16 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsFromJSON from "./contacts.json";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state= {
+    contacts: contactsFromJSON.slice(0,5)
+  }
+
+// 
+  addRandomContact = ()=> {
+    const randomNumber = Math.floor(Math.random() * (contactsFromJSON.length - 5) + 5);
+    const randomContact = contactsFromJSON[randomNumber];
+
+    this.setState({
+      contacts: this.state.contacts.concat(randomContact)
+    });
+  }
+
+  sortByName = () => {
+    const {contacts} = this.state;
+
+    function compare(a, b) {
+      const contactA = a.name;
+      const contactB = b.name;
+    
+      let comparison = 0;
+      if (contactA > contactB) {
+        comparison = 1;
+      } else if (contactA < contactB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+    
+    const sortedContactsByName = contacts.sort(compare);
+
+    this.setState({
+      contacts: sortedContactsByName
+    });
+  }
+
+  sortByPopularity = () => {
+    const {contacts} = this.state;
+
+    function compare(a, b) {
+      const contactA = a.popularity;
+      const contactB = b.popularity;
+    
+      let comparison = 0;
+      if (contactA > contactB) {
+        comparison = 1;
+      } else if (contactA < contactB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+    
+    const sortedContactsByPopularity = contacts.sort(compare);
+
+    this.setState({
+      contacts: sortedContactsByPopularity
+    });
+  }
+
+  deleteContact = (id)=> {
+    this.setState({
+      contacts: this.state.contacts.filter((contact) => contact.id !== id)
+  });
+  }
+
+  render() {
+    const {contacts} = this.state;
+    return (
+      <div className="App">
+        <h1> Ironcontacts </h1>
+        <button onClick={this.addRandomContact}> Add random contact </button>
+        <button onClick={this.sortByName}> Sort by name </button>
+        <button onClick={this.sortByPopularity}> Sort by popularity </button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+          {contacts.map((contact) => {
+            
+              return (
+                <>
+                <tr>
+                  <td> <img src={contact.pictureUrl} alt="" style={{height:75, width:50}}/> </td>
+                  <td> {contact.name} </td>
+                  <td> {contact.popularity} </td>
+                  <td> <button onClick={() => this.deleteContact(contact.id)}> Delete </button> </td>
+                </tr>
+                </>
+                );
+            })}
+
+        </table>
+      </div>
+    );
+  }
+  
+
 }
 
 export default App;
